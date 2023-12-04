5f6e3838b9684846fdc9ac2a61a239277232c16f
vishnu suthar <sutharvishnu2021@gmail.com>
2023-12-04 22:59:49 +0530

    custom DB connection

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constant";
-// import express from "express";
-// const app = express();
+import express from "express";
+const app = express();
 
 import dotenv from "dotenv";
 import connectDB from "./db/dbConnect.js";
 dotenv.config({ path: "./env" });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 7000, () => {
+      console.log(`server started at port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!! ", error);
+  });
 
 // (async () => {
 //   try {
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+//doing same thing using two different way first using promises and second using try/catch
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(error.code || 500).json({
+//       success: false,
+//       message: error.message,
+//     });
+//   }
+// };
+export { asyncHandler };
